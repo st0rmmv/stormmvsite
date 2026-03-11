bf3cfc79809a6265463ce0272467ed7c0d207ea5
Storm <desvelasquez19@gmail.com>
2026-03-11 02:24:17 -0700

    updated project pages

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("style.css");   // your CSS
   eleventyConfig.addPassthroughCopy("icons");       // your icons folder
 eleventyConfig.addPassthroughCopy("gamesimage");       // your icons folder 2
+eleventyConfig.addPassthroughCopy("gallery");       // your icons 
 eleventyConfig.addPassthroughCopy("pagea");       // folder 3
 eleventyConfig.addPassthroughCopy("pagec");       // folder 4
 
